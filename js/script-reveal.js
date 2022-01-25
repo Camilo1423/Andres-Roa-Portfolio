69fa027f5e3428d9204bc6661575a3f2c8e51364
Andres <AndresMore1423@hotmail.com>
2022-01-25 02:55:52 -0500

    Mi primer commit

---
+++ js/script-reveal.js
@@ -0,0 +1,80 @@
+window.sr = ScrollReveal(); 
+
+    sr.reveal ('.img-mine', {
+            duration: 5000,
+            origin: 'right',
+            distance: '-50px'
+
+    });
+    
+    sr.reveal ('.text-1', {
+        duration: 3000,
+        origin: 'left',
+        distance: '-100px',
+        delay: 800,
+
+    });
+
+    sr.reveal ('.text-2', {
+        duration: 3000,
+        origin: 'left',
+        distance: '-100px',
+        delay: 1300,
+    });
+
+    sr.reveal ('.text-3', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-100px',
+        delay: 1300,
+    });
+
+    sr.reveal ('.text-4', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-100px',
+        delay: 1800,
+    });
+
+    sr.reveal ('.text-5', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-100px',
+        delay: 2100,
+    });
+
+    sr.reveal ('.text-6', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px',
+        delay: 1300,
+    });
+
+    sr.reveal ('.text-7', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px',
+        delay: 1800,
+    });
+
+    sr.reveal ('.text-8', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px',
+        delay: 2100,
+    });
+
+    sr.reveal ('.text-9', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px',
+        delay: 2300,
+    });
+
+    sr.reveal ('.text-10', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px',
+        delay: 2600,
+    });
+
