3ade84546b392dbd97132b52a6630d152b858fa6
Andres <AndresMore1423@hotmail.com>
2022-02-09 15:30:53 -0500

    merjarmiento del resposive

--- js/script-nav-bar.js
+++ js/script-nav-bar.js
@@ -49,7 +49,7 @@ $(".rotate").textrotator({
 
 	  localStorage.setItem("saved-theme", getCurrentTheme());
 	  localStorage.setItem("saved-icon", getCurrentIcon());
-  })
+  });
 
   const getCurrentTheme = () => document.body.classList.contains("dark-theme") ? "dark" : "light";
   const getCurrentIcon = () => DarkMood.classList.contains("sun") ? "sun" : "moon";
@@ -57,7 +57,27 @@ $(".rotate").textrotator({
   const savedTheme = localStorage.getItem("saved-theme");
   const savedIcon = localStorage.getItem("saved-icon");
 
-  if(saveTheme) {
+  if(savedTheme) {
 	  document.body.classList[savedTheme === "dark" ? "add" : "remove"]("dark-theme");
-	  DarkMood.classList[savedIcon === "sun" ? "add" : "revome"]("sun");
-  }
+  };
+
+  /* menu toggle */
+
+  const menuBtn = document.querySelector(".btn-responsive");
+  const closeBtn = document.querySelector(".nav-close-btn");
+  const navigation = document.querySelector(".menu-bar");
+  const navItems = document.querySelectorAll(".nav-initial a");
+
+  menuBtn.addEventListener("click", () => {
+      navigation.classList.add("active");
+  });
+  
+  closeBtn.addEventListener("click", () => {
+    navigation.classList.remove("active");
+  });
+  
+  navItems.forEach((navItem) => {
+    navItem.addEventListener("click", () => {
+      navigation.classList.remove("active");
+    })
+  })
